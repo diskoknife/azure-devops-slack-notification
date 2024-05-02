6bfc4f1a2fd6d75db76938cd2e35cd42eef779b7
Viacheslav Shchurov <diskoknife@gmail.com>
2024-05-02 19:55:27 +0200

    first version of converting file

---
+++ scripts/json_to_csv.py
@@ -0,0 +1,45 @@
+import os
+import json
+import csv
+
+json_var_file=os.getenv("JSON_VARIABLES_GROUPS")
+devops_releases=os.getenv("JSON_OUTPUT_FILE")
+csv_output_file=os.getenv("CSV_OUTPUT_FILE")
+
+
+def generate_csv(release_file, csv_output_file, variable_group_file):
+    """
+
+    First, we parse data from bash-generated json line by line.
+    loads() works with string, load() with objects.
+    Next we iterate over dictionary first appending release to array next release stages to it and so on over every line
+    Next we determine what variable groups are exist in our scope and where are they used
+
+    """
+    with open(release_file, 'r') as json_data:
+        data = []
+        for line in json_data:
+            data.append(json.loads(line))
+
+    releases_and_stages = []
+    for item in data:
+        release = item['release']
+        stages = [stage['stage'] for stage in item['stages']]
+        releases_and_stages.append(release)
+        releases_and_stages.extend(stages)
+
+    with open(variable_group_file) as var_json:
+        variable_groups = json.load(var_json)
+
+    #defining header row in csv data
+    csv_data = [[""] + releases_and_stages]
+    
+    for group in variable_groups:
+        row = [group] + [group in item.get("releaseVariableGroups", [])] + [group in stage.get("stageVariableGroup", []) for stage in item["stages"]]
+        csv_data.append(row)
+    with open(csv_output_file, 'w') as csv_out:
+        writer = csv.writer(csv_out)
+        writer.writerows(csv_data)
+
+generate_csv(devops_releases, csv_output_file, json_var_file)
+
